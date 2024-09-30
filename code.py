f6542a14b20701dad2a81270e79353eae07f03a3
Esha <67066799+Eshaaaaa@users.noreply.github.com>
2024-10-01 01:17:32 +0530

    Update code.py

--- code.py
+++ code.py
@@ -1,7 +1,3 @@
-# This Python 3 environment comes with many helpful analytics libraries installed
-# It is defined by the kaggle/python Docker image: https://github.com/kaggle/docker-python
-# For example, here's several helpful packages to load
-
 import numpy as np # linear algebra
 import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
 import matplotlib.pyplot as plt
@@ -14,17 +10,10 @@ from sklearn.metrics import confusion_matrix
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split,GridSearchCV,cross_val_score
 
-%matplotlib inline
-# Input data files are available in the read-only "../input/" directory
-# For example, running this (by clicking run or pressing Shift+Enter) will list all files under the input directory
-
 import os
 for dirname, _, filenames in os.walk('D:\3 rd yr\Mini_Project'):
     for filename in filenames:
         print(os.path.join(dirname, filename))
-
-# You can write up to 20GB to the current directory (/kaggle/working/) that gets preserved as output when you create a version using "Save & Run All" 
-# You can also write temporary files to /kaggle/temp/, but they won't be saved outside of the current session
 
 df = pd.read_csv('D:/3 rd yr/Mini_Project/credit/bankloans.csv')
 df.head()
